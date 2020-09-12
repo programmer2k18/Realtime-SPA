395ac7eac3b2a0c0216bbc10d9399481b621fe87
programmer2k18 <programmer2k18@gmail.com>
2020-09-12 10:43:30 +0200

    created model, resource controller, factory, seeder, migration for App models

---
+++ database/migrations/2020_09_12_081241_create_questions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->text('body');
+            $table->integer('user_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+
+            $table->foreign('user_id')->references('id')
+                ->on('users')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->foreign('category_id')->references('id')
+                ->on('categories')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
